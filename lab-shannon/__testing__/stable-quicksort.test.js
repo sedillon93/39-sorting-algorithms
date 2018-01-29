0f50e2c7820a48a1de79b10558acb6e5c5ef2969
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-28 18:13:02 -0800

    add testing to see if isSorted returns true for the arrays in quicksort

--- lab-shannon/__testing__/stable-quicksort.test.js
+++ lab-shannon/__testing__/stable-quicksort.test.js
@@ -5,21 +5,26 @@ describe(`stable quicksort`, () => {
   test(`empty array`, () => {
     let collection = testingFunctions.generateCollection(0);
     expect(stableQuicksort(collection)).toEqual([]);
+    expect(testingFunctions.isSorted(stableQuicksort(collection))).toBeTruthy();
   });
   test(`single element array`, () => {
     let collection = testingFunctions.generateCollection(1);
     expect(stableQuicksort(collection)).toEqual(collection);
+    expect(testingFunctions.isSorted(stableQuicksort(collection))).toBeTruthy();
   });
   test(`small array`, () => {
     let collection = testingFunctions.generateCollection(5);
     expect(stableQuicksort(collection)).toEqual(collection.sort(function(a,b){return a - b;}));
+    expect(testingFunctions.isSorted(stableQuicksort(collection))).toBeTruthy();
   });
   test(`medium array`, () => {
     let collection = testingFunctions.generateCollection(100);
     expect(stableQuicksort(collection)).toEqual(collection.sort(function(a,b){return a - b;}));
+    expect(testingFunctions.isSorted(stableQuicksort(collection))).toBeTruthy();
   });
   test(`large array`, () => {
     let collection = testingFunctions.generateCollection(5000);
     expect(stableQuicksort(collection)).toEqual(collection.sort(function(a,b){return a - b;}));
+    expect(testingFunctions.isSorted(stableQuicksort(collection))).toBeTruthy();
   });
 });
